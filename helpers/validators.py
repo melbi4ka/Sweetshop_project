06a9e5b799026e94ef00be2be9d2a497e389e33f
Maya Bachvarova <may13@abv.bg>
2022-12-14 21:45:16 +0200

    changes

--- helpers/validators.py
+++ helpers/validators.py
@@ -7,12 +7,3 @@ def validate_alphabet_characters(value):
         if not char.isalpha() and char != " ":
             raise exceptions.ValidationError('Only letters are allowed')
 
-# def validate_only_letters(value):
-#     for ch in value:
-#         if not ch.isalpha():
-#             raise exceptions.ValidationError('Only letters are allowed')
-#
-# def validate_only_digits(value):
-#     for ch in value:
-#         if not ch.isdigit():
-#             raise exceptions.ValidationError('Only digits are allowed')
